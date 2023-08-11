2f4ae5022715946d4111ea6e5044b8add09962a2
Ashwin Raj C K <ashwinraj275@gmail.com>
2023-08-11 15:19:07 +0530

    asgi setting bug fix

--- project/project/asgi.py
+++ project/project/asgi.py
@@ -7,7 +7,7 @@ from django.core.asgi import get_asgi_application
 
 from chat.routing import websocket_urlpatterns
 from chat.middleware import TokenAuthMiddleware
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
