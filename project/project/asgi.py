0c24dd0ece78828e772a12c73bac1ef92a3f4761
Ashwin Raj C K <ashwinraj275@gmail.com>
2023-08-11 19:53:28 +0530

    asgi setting bug fix

--- project/project/asgi.py
+++ project/project/asgi.py
@@ -2,23 +2,21 @@ import os
 
 from channels.auth import AuthMiddlewareStack  #type:ignore
 from channels.routing import ProtocolTypeRouter, URLRouter  #type:ignore
-from channels.security.websocket import AllowedHostsOriginValidator  #type:ignore
 from django.core.asgi import get_asgi_application
 
-from chat.routing import websocket_urlpatterns
-from chat.middleware import TokenAuthMiddleware
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
 django_asgi_app = get_asgi_application()
 
 import chat.routing
 
+
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": 
+        "websocket":
             AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
-       
+
     }
 )
