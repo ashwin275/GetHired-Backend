5cfff0304f2877d7f4d84ec875ddd6de9f671cae
Ashwin Raj C K <ashwinraj275@gmail.com>
2023-08-12 11:12:43 +0530

    asgi setting bug fix

--- project/project/asgi.py
+++ project/project/asgi.py
@@ -5,7 +5,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter  #type:ignore
 from django.core.asgi import get_asgi_application
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 django_asgi_app = get_asgi_application()
 
